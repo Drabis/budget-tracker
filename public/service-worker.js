bfcb3c80cd0b8b5cb41e36133ac73dba17ae1d6d
Drabis <drabis47@gmail.com>
2021-06-06 22:20:48 -0400

    created db.js

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/style.css",
+  "/index.js",
+  "/manifest.webmanifest.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  
+];
+
+const CACHE_NAME = "mysite-v1";
+const DATA_CAHE_NAME = "data-cahe-v1";
+
